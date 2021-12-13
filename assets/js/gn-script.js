57b16728d31413258c2022013d87ba703dde2271
Jonnywdev <jonnywhitt19@gmail.com>
2021-12-13 14:56:58 +0000

    Set initial structure for js, add all functions

---
+++ assets/js/gn-script.js
@@ -0,0 +1,16 @@
+
+
+
+
+function startGame()
+function setNextQuestion()
+function showQuestion()
+function resetButtons()
+function selectAnswer()
+function setCardClass()
+function clearCardClass()
+function incrementScore()
+function incrementWrongAnswer()
+function incrementQuestionCount()
+
+
