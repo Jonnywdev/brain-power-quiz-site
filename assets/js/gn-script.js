feddfba4ecd09b5a79a7b9c318123e8975d72a3c
Jonnywdev <jonnywhitt19@gmail.com>
2021-12-13 15:05:06 +0000

    Add comments to each function

--- assets/js/gn-script.js
+++ assets/js/gn-script.js
@@ -1,16 +1,64 @@
 
 
 
-
+/**
+ * Starts the game once 
+ * start game button has been clicked
+ */
 function startGame()
+
+/**
+ * Sets the next question. 
+ * from the list of questions below.
+ */
 function setNextQuestion()
+
+/**
+ * Shows the question on the card.
+ */
 function showQuestion()
-function resetButtons()
+
+/**
+ * Sets the correct answer on the screen.
+ * changes the colors, the correct button will change
+ * to green, the incorrect answers will turn to red.
+ */
 function selectAnswer()
-function setCardClass()
-function clearCardClass()
+
+/**
+ * Resets the answer buttons,
+ * back to their original color.
+ */
+function resetButtons()
+
+/**
+ * Sets the background to either
+ * red or green, depending on 
+ * whether you have selected the correct or 
+ * incorrect answer.
+ */
+function setBackClass()
+
+/**
+ * Changes the background color back to the original.
+ */
+function clearBackClass()
+
+/**
+ * Adds 1 to the score if you get the answer right!
+ */
 function incrementScore()
+
+/**
+ * Adds 1 to the wrong answer box if you dont get the 
+ * answer correct.
+ */
 function incrementWrongAnswer()
+
+/**
+ * Adds 1 to the question count,
+ * as you go through the questions.
+ */
 function incrementQuestionCount()
 
 
