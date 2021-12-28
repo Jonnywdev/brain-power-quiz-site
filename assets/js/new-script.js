cf864895305a2d98c90238d67d3312f54a986382
Jonnywdev <jonnywhitt19@gmail.com>
2021-12-28 18:08:31 +0000

    Add list of const declarations needed

--- assets/js/new-script.js
+++ assets/js/new-script.js
@@ -1,6 +1,10 @@
 const startButton = document.getElementById('start-btn')
 const welcomeScreen = document.getElementById('welcome--card')
 const questionWrapper = document.getElementById('quiz-wrapper')
+const choices = Array.from(document.getElementsByClassName('choice-text'))
+const progressText = document.getElementById('progressText')
+const scoreText = document.getElementById('score')
+const progressBarFull = document.getElementById('progressBarFull')
 
 startButton.addEventListener('click', startGame)
 
